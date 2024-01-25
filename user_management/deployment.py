d2a69b53d4be0e5af16efff47d18f3af19ff1421
raghudeveloptrees <raghudeveloptrees@gmail.com>
2024-01-25 14:40:35 +0530

    removed connection strings

--- user_management/deployment.py
+++ user_management/deployment.py
@@ -24,15 +24,12 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-conn_str = os.environ['AZURE_MYSQL_CONNECTIONSTRING']
-print (conn_str)
-conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': conn_str_params['dbname'],
-        'HOST': conn_str_params['host'],
-        'USER': conn_str_params['user'],
-        'PASSWORD': conn_str_params['password'],
+        'NAME': os.environ['AZURE_MYSQL_NAME'],
+        'HOST': os.environ['AZURE_MYSQL_HOST'],
+        'USER': os.environ['AZURE_MYSQL_USER'],
+        'PASSWORD': os.environ['AZURE_MYSQL_PASSWORD'],
     }
 }
